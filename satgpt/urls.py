3200131d07e6a8b88475910176408762fc9d6246
rezasdsju <rezaul2002104@gmail.com>
2025-09-07 23:05:12 +0600

    commit to update satgpt -00

--- satgpt/urls.py
+++ satgpt/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.chat_view, name="satgpt"),
     path("get-response/", views.get_response, name="satgpt_response"),
+    path("clear-chat/", views.clear_chat_history, name="clear_chat_history"),
 ]
